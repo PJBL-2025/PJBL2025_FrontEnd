ae4b11dba6ab0517e5ee2051622a39d41dbd9215
Rifqiww <rifqinabil378@gmail.com>
2025-04-11 11:05:35 +0700

    Add Footer

--- Pages/Homepage.tsx
+++ Pages/Homepage.tsx
@@ -1,6 +1,8 @@
 import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Footer from '../components/Footer';
 
+// Category
 const getCategoryIcon = (iconName: string) => {
   switch (iconName) {
     case 'promo':
@@ -17,13 +19,12 @@ const getCategoryIcon = (iconName: string) => {
       return { source: require('../assets/icons/promo.png'), size: 40 };
   }
 };
-
 const CategoryButton = ({ icon, label }: { icon: string; label: string }) => {
   const { source, size } = getCategoryIcon(icon);
 
   return (
     <TouchableOpacity className="items-center justify-center mx-2 self-center">
-      <View className="w-12 h-12 bg-gray-100 rounded-full items-center justify-center mb-1">
+      <View className="w-16 h-16 bg-gray-100 rounded-full items-center justify-center mb-1">
         <Image 
           source={source}
           style={{ width: size, height: size }}
@@ -44,21 +45,45 @@ const formatRupiah = (price: number) => {
   }).format(price);
 };
 
-const ProductCard = ({ title, price, reviews }: {
+// PRODUCT CARD
+const ProductCard = ({
+  title,
+  price,
+  reviews,
+  image,
+}: {
   title: string;
   price: number;
   reviews: number;
+  image: any;
 }) => (
-  <TouchableOpacity className="w-[48%] bg-white rounded-xl p-3 mb-4 shadow-sm">
-    <View className="w-full h-32 bg-gray-100 rounded-lg mb-2 items-center justify-center">
-      <Text className="text-gray-400">Product Image</Text>
+  <TouchableOpacity
+    className="w-[48%] bg-white rounded-2xl p-3 mb-4"
+    style={{
+      shadowColor: '#687582',
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.2,
+      shadowRadius: 30,
+      elevation: 4,
+    }}
+  >
+    <View className="w-full h-32 rounded-xl bg-white items-center justify-center overflow-hidden mb-3">
+      <Image
+        source={image}
+        style={{ width: '100%', height: '100%' }}
+        resizeMode="contain"
+      />
     </View>
-    <Text className="text-sm font-medium mb-1">{title}</Text>
-    <Text className="text-sm font-bold">{formatRupiah(price)}</Text>
-    <View className="flex-row items-center mt-1">
-      <Text className="text-yellow-500">★</Text>
-      <Text className="text-xs text-gray-600 ml-1">4.8</Text>
-      <Text className="text-xs text-gray-400 ml-1">({reviews} Reviews)</Text>
+    <Text className="text-sm font-medium text-black mb-1">{title}</Text>
+    <Text className="text-sm font-bold text-black">{formatRupiah(price)}</Text>
+    <View className="flex-row items-center mt-1 space-x-1">
+      <Image
+        source={require('../assets/icons/star.png')}
+        className="w-4 h-4"
+        resizeMode="contain"
+      />
+      <Text className="text-xs text-gray-600">4.6</Text>
+      <Text className="text-xs text-gray-400">( {reviews} Reviews )</Text>
     </View>
   </TouchableOpacity>
 );
@@ -137,34 +162,60 @@ export default function Homepage() {
             style={{ width: 100, height: 200 }}
             resizeMode="contain"
           />
-        </View>
-          <View className="w-64 h-32 bg-gray-100 rounded-xl p-4 mr-4">
-            <Text className="text-lg font-bold mb-1">TMA-2 Modular Headphone</Text>
+          </View>
+          <View className="w-64 h-32 bg-white rounded-xl px-4 py-3 mr-4 flex-row items-center justify-between">
+          <View>
+            <Text className="text-black text-base font-bold mb-2">
+              TMA-2{'\n'}Modular{'\n'}Headphone
+            </Text>
             <TouchableOpacity>
-              <Text className="text-blue-500">Shop now →</Text>
+              <Text className="text-white font-medium">
+              <Text className="text-blue-500">Shop now→</Text>
+              </Text>
             </TouchableOpacity>
+          </View>
+          
+          <Image
+            source={require('../assets/images/headphone.png')}
+            style={{ width: 100, height: 200 }}
+            resizeMode="contain"
+          />
+          </View>
+          <View className="w-64 h-32 bg-white rounded-xl px-4 py-3 mr-4 flex-row items-center justify-between">
+          <View>
+            <Text className="text-black text-base font-bold mb-2">
+              TMA-2{'\n'}Modular{'\n'}Headphone
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-white font-medium">
+              <Text className="text-blue-500">Shop now→</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+          
+          <Image
+            source={require('../assets/images/headphone.png')}
+            style={{ width: 100, height: 200 }}
+            resizeMode="contain"
+          />
           </View>
         </ScrollView>
 
         {/* Categories */}
-        <ScrollView 
-          horizontal 
-          showsHorizontalScrollIndicator={false}
-          className="px-4 mb-4"
-          contentContainerStyle={{ alignItems: 'center'}}
-        >
+        <View className="flex-row justify-center items-center space-x-4 mb-4">
           <CategoryButton icon="promo" label="Promo" />
           <CategoryButton icon="clothes" label="Clothes" />
           <CategoryButton icon="hat" label="Hat" />
           <CategoryButton icon="pants" label="Pants" />
           <CategoryButton icon="design" label="Design" />
-        </ScrollView>
+        </View>
         </View>
 
         {/* Featured Products */}
-        <View className="px-4 pb-4">
+        <View className="bg-blue-500">
+          <View className="bg-white px-4 pt-6 rounded-t-2xl">
           <View className="flex-row justify-between items-center mb-4">
-            <Text className="text-lg font-bold">Featured Products</Text>
+            <Text className="ml-4 text-lg font-bold">Featured Products</Text>
             <TouchableOpacity>
               <Text className="text-blue-500">See All</Text>
             </TouchableOpacity>
@@ -175,24 +226,168 @@ export default function Homepage() {
               title="TMA-2 HD Wireless"
               price={2000000}
               reviews={86}
+              image={require('../assets/images/headphone.png')}
             />
             <ProductCard 
               title="TMA-2 HD Wireless"
               price={1500000}
               reviews={86}
+              image={require('../assets/images/headphone.png')}
             />
             <ProductCard 
               title="TMA-2 HD Wireless"
               price={1800000}
               reviews={86}
+              image={require('../assets/images/headphone.png')}
             />
             <ProductCard 
               title="TMA-2 HD Wireless"
               price={2500000}
               reviews={86}
+              image={require('../assets/images/headphone.png')}
             />
           </View>
+          </View>
         </View>
+
+      <View
+      className="bg-white px-6 py-3 rounded-xl items-center justify-center m-8"
+      style={{
+        // iOS shadow
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        // Android shadow
+        elevation: 4,
+          }}
+        >
+        <Text className="text-black text-lg font-semibold">Clothes</Text>
+      </View>
+
+      {/* Scroll Content */}
+      <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          className="px-4 py-4"
+        >
+          <View className="w-64 h-32 bg-white rounded-xl px-4 py-3 mr-4 flex-row items-center justify-between"
+          style={{
+            // iOS shadow
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            // Android shadow
+            elevation: 4,
+              }}
+          >
+          <View>
+            <Text className="text-black text-base font-bold mb-2">
+              TMA-2{'\n'}Modular{'\n'}Headphone
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-white font-medium">
+              <Text className="text-blue-500">Shop now→</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+          
+          <Image
+            source={require('../assets/images/headphone.png')}
+            style={{ width: 100, height: 200 }}
+            resizeMode="contain"
+          />
+          </View>
+          <View className="w-64 h-32 bg-white rounded-xl px-4 py-3 mr-4 flex-row items-center justify-between"
+          style={{
+            // iOS shadow
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            // Android shadow
+            elevation: 4,
+              }}
+          >
+          <View>
+            <Text className="text-black text-base font-bold mb-2">
+              TMA-2{'\n'}Modular{'\n'}Headphone
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-white font-medium">
+              <Text className="text-blue-500">Shop now→</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+          
+          <Image
+            source={require('../assets/images/headphone.png')}
+            style={{ width: 100, height: 200 }}
+            resizeMode="contain"
+          />
+          </View>
+          <View className="w-64 h-32 bg-white rounded-xl px-4 py-3 mr-4 flex-row items-center justify-between"
+          style={{
+            // iOS shadow
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 3 },
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            // Android shadow
+            elevation: 4,
+              }}
+          >
+          <View>
+            <Text className="text-black text-base font-bold mb-2">
+              TMA-2{'\n'}Modular{'\n'}Headphone
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-white font-medium">
+              <Text className="text-blue-500">Shop now→</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+          
+          <Image
+            source={require('../assets/images/headphone.png')}
+            style={{ width: 100, height: 200 }}
+            resizeMode="contain"
+          />
+          </View>
+        </ScrollView>
+        
+        <View>
+          <View className="px-4 pt-6">
+          <View className="flex-row flex-wrap justify-between">
+            <ProductCard 
+              title="TMA-2 HD Wireless"
+              price={2000000}
+              reviews={86}
+              image={require('../assets/images/headphone.png')}
+            />
+            <ProductCard 
+              title="TMA-2 HD Wireless"
+              price={1500000}
+              reviews={86}
+              image={require('../assets/images/headphone.png')}
+            />
+            <ProductCard 
+              title="TMA-2 HD Wireless"
+              price={1800000}
+              reviews={86}
+              image={require('../assets/images/headphone.png')}
+            />
+            <ProductCard 
+              title="TMA-2 HD Wireless"
+              price={2500000}
+              reviews={86}
+              image={require('../assets/images/headphone.png')}
+            />
+          </View>
+          </View>
+        </View>
+      <Footer/>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,57 @@
+import { View, Text, Image } from 'react-native';
+
+const Footer = () => {
+  return (
+    <View className="bg-white px-4 pt-6 pb-4 space-y-6">
+        <View className='linear-gradient'>
+        </View>
+      {/* Section 1: Customer Service + Text */}
+      <View className="flex-row justify-between">
+        {/* Kiri: Icon dan Teks */}
+        <View className="space-y-3">
+          <View className="flex-row items-center space-x-2">
+            <Image source={require('../assets/icons/customer-service.png')} className="w-5 h-5" />
+            <Text className="text-gray-600 text-[10px]">Customer Service</Text>
+          </View>
+          <View className="flex-row items-center space-x-2">
+            <Image source={require('../assets/icons/thumb.png')} className="w-5 h-5" />
+            <Text className="text-gray-600 text-[10px]">Criticism and suggestions</Text>
+          </View>
+        </View>
+
+        {/* Kanan: Paragraf */}
+        <Text className="text-gray-400 w-[48%] text-left text-[12px]">
+          malesuada eget consectetur non non laoreet Donec nisl. non, ultrices quam Nam tincidunt faucibus nulla, Sed ipsum Lorem sit
+        </Text>
+      </View>
+
+      <View className="flex-row justify-between px-6 mt-10">
+        {/* Kolom 1: Follow Us */}
+        <View className="flex-1 items-start">
+            <Text className="font-bold text-lg mb-2">Follow Us</Text>
+            <View className="flex-row space-x-2">
+            <Image source={require('../assets/icons/instagram.png')} className="w-7 h-7" />
+            <Image source={require('../assets/icons/tiktok.png')} className="w-7 h-7" />
+            </View>
+        </View>
+
+        {/* Kolom 2: Payment */}
+        <View className="flex-1 items-center">
+            <Text className="font-bold text-lg mb-[-52]">Payment</Text>
+            <View className="items-center">
+            <Image
+                source={require('../assets/images/payment.png')}
+                className="w-[200px] h-[200px]"
+                style={{ resizeMode: 'contain' }}
+            />
+            </View>
+        </View>
+        </View>
+
+      {/* Section 3: Footer Text */}
+      <Text className="text-center text-gray-400 mt-4 text-xs">Since 2025</Text>
+    </View>
+  );
+};
+
+export default Footer;
