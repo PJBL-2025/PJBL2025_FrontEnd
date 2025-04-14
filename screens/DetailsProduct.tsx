d226dd4fe3a3a73fa2c6590bba87ac63a9690f77
Mahes <mahesavpm123@gmail.com>
2025-04-14 12:48:06 +0700

    add_DetailsProduct

---
+++ screens/DetailsProduct.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const DetailsProduct = () => {
+    return(
+        <View>
+            <Text>
+                Welcome
+            </Text>
+        </View>
+    )
+}
+
+export default DetailsProduct
